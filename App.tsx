0b368ad2a99c5d1ce956f0edb04c35c8ec49675e
Rafael Kasper <prof.rafaelkasper@gmail.com>
2024-06-28 21:46:36 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { RootSiblingParent } from "react-native-root-siblings";
+import Routes from "./src/routes";
+import React from "react";
+import { UserProvider } from "./src/contexts/UserContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootSiblingParent>
+      <UserProvider>
+        <Routes />
+      </UserProvider>
+    </RootSiblingParent>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/types/Product.ts
@@ -0,0 +1,14 @@
+interface ProductDTO {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+}
+
+interface Rating {
+  rate: number;
+  count: number;
+}
